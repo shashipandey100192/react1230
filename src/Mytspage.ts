43959af50d7ace74a807062119542c37e6ffdb6f
shashiweb <shashiweb64@gmail.com>
2024-12-26 13:16:58 +0530

    change

--- src/Mytspage.ts
+++
@@ -1,6 +0,0 @@
-import react from "react";
-
-function Abc() {
-    return `<h1>welcome to ts</h1>`;
-}
-export default Abc
--- src/index.js
+++ src/index.js
@@ -1,31 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Xys, { About, About2 } from './Myabout';
-import Mycontact from './Mycontact';
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
-import "./style.css";
-import UUU from './Mytspage.ts';
-import Mycontactus from './Contactus.js';
-import Myreactbootstrap from './Myreactbootstrap.js';
-import Mymuipage from './Mymuipage.js';
-import Myreacticons from './Myreacticons.js';
+
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Myreacticons></Myreacticons>
-    <Myreactbootstrap/>
-    <Mymuipage/>
-    <h1>sdfhsdjkfhj</h1>
-    <div>sdjfhjsdhf sdfsdfsdfds</div>
-    <About></About>
-    <Xys></Xys>
-    <About2></About2>
-    <Mycontact/>
-    <UUU/>
-  <Mycontactus></Mycontactus>
+    
   </React.StrictMode>
 );
 
